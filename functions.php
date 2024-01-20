e1df9e36d860da391f426b87f346575be72de373
Ford Henley <info@fordswebhelp.com>
2024-01-20 17:57:50 -0600

    Began Setup of Base Theme Structure
    git checkout -b DP-2-setup

--- functions.php
+++ functions.php
@@ -143,6 +143,17 @@ function the_defining_point_scripts() {
 
 	wp_enqueue_script( 'the-defining-point-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
+	// Adding Bootstrap to the Theme - Start
+
+	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
+	wp_enqueue_script( 'bootstrap-js' , get_template_directory_uri() . '/bootstrap/js/bootstrap.bundle.min.js', array('jquery') );
+
+	// Adding Bootstrap to the Theme - Start
+
+	// Theme CSS
+
+	wp_enqueue_style( 'theme-style', get_template_directory_uri() . '/css/theme_style.css' );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
--- header.php
+++ header.php
@@ -24,36 +24,29 @@
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'the-defining-point' ); ?></a>
-
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$the_defining_point_description = get_bloginfo( 'description', 'display' );
-			if ( $the_defining_point_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $the_defining_point_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'the-defining-point' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
+	<nav class="navbar navbar-expand-lg">
+  		<div class="container-fluid">
+		  <a class="navbar-brand" href="#">
+		  <?php
+			the_custom_logo(); 
 			?>
-		</nav><!-- #site-navigation -->
+		</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+			<ul class="navbar-nav">
+			<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+							'menu_class' => 'navbar-nav',
+						)
+					);
+					?>
+			</ul>
+			</div>
+		</div>
 	</header><!-- #masthead -->
