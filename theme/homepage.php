fa34ab9d72c4e4526b9c5cf7a9a6ec7dff4837b6
Ford Henley <info@fordswebhelp.com>
2024-01-20 16:49:03 -0600

    Updated Homepage
    git checkout -b DP-2-setup

--- theme/homepage.php
+++ theme/homepage.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for Defining Point Homepage Template
  *
  * This is the template that displays all pages by default. Please note that
  * this is the WordPress construct of pages: specifically, posts with a post
